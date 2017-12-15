e8d5749aa8b6eaa5512827066486d5499e5067dd
Calvin Sykes <sykescalvin09@gmail.com>
2017-12-15 15:59:42 +0000

    mess; committing to push to zwicky

--- jupyter-playground/rhogas.py
+++ jupyter-playground/rhogas.py
@@ -41,37 +41,48 @@ rhocrit = 3 * (100 * h * 1e5 / (cm_to_pc * 1e6))**2 / (8 * np.pi * G)
 ####################
 
 def rho(lognh):
+    """
+    Convert log10(number density) to physical density in g cm^-3
+    """
     return 10**lognh * mp
 
 class DensityTempRelation:
+    """
+    Encapsulates a density-temperature relation obtained from a text file containing a list of paired log(n_H), log(T) values.
+    Loads the data and constructs an interpolation T(n_H) and its derivative dT/dn_H
+    """
+
     def __init__(self, model_str):
+        """
+        Construct a density-temperature relation.
+        
+        model_str: String containing the name of the data file to load. The path searched is 'data/nH_T_' + model_str + '.dat'
+        """
         try:
             log_nH, log_T = np.loadtxt('data/nH_T_{:s}.dat'.format(model_str.lower()), unpack=True)
         except IOError:
             print('{:s} density-temperature relation data not found\nAborting...'.format(model_str))
-            exit()
+            exit(1)
         else:
             print('Loading {:s} density-temperature parameter relation data'.format(model_str.title()))
-            self.logTfunc = interpolate.UnivariateSpline(log_nH, log_T, s=0.001, k=3)
-            self.dlogTfunc = self.logTfunc.derivative()
+            self.__logTfunc = interpolate.UnivariateSpline(log_nH, log_T, s=0.001, k=3)
+            self.__dlogTfunc = self.__logTfunc.derivative()
 
     def T(self, lognh):
-        return 10**self.logTfunc(lognh)
+        """
+        Return the temperature for a given log10(number density)
+        """
+        return 10**self.__logTfunc(lognh)
 
     def dT_drho(self, lognh):
-        return (self.logTfunc(lognh) / rho(lognh)) * self.dlogTfunc(lognh)
+        """
+        Return the derivative of temperature wrt number density for a given log10(number density)
+        """
+        return (self.T(lognh) / rho(lognh)) * self.__dlogTfunc(lognh)
 
+# Global variable to hold the density-temperature relation object.
+# Defined now so that it is visible to functions which need to use it.
 T_rel = None
-
-#def make_nH_T_interp(model_str):
-#    try:
-#        log_nH, log_T = np.loadtxt('data/nH_T_{:s}.dat'.format(model_str.lower()), unpack=True)
-#    except IOError:
-#        print('{:s} density-temperature relation data not found\nAborting...'.format(model_str))
-#        exit()
-#    else:
-#        print('Loading {:s} density-temperature parameter relation data'.format(model_str.title()))
-#    return interpolate.UnivariateSpline(log_nH, log_T, s=0.001, k=3)
 
 ################
 ## F function ##
@@ -81,12 +92,21 @@ T_rel = None
 log_mean_nh = -6.7
 
 def F_integrand(lognh):
+    """
+    Compute the value of T/rho + dT/drho for a given log10(number density).
+    """
     log_int =  T_rel.T(lognh) / rho(lognh) + T_rel.dT_drho(lognh)
     # The integral is computed in log(nH) space
-    # so a conversion back into rho space is required 
-    return log_int * 10**lognh * mp * 2.303 # ln(10)
+    # so a conversion back into rho space is required
+    # 2.303 = ln(10)
+    return log_int * 10**lognh * mp * 2.303
 
 def Ff(lognh):
+    """
+    Numerically integrate the F_integrand function, with lower limit given by the mean background density.
+
+    lognh: The upper limit of integration.
+    """
     return integrate.quad(F_integrand, log_mean_nh, lognh)[0]
 
 #######################
@@ -94,9 +114,23 @@ def Ff(lognh):
 #######################
 
 def Rvir(Mvir):
+    """
+    Calculate the virial radius for a given virial mass.
+
+    Mvir: The virial mass in units of solar masses.
+
+    Returns: The virial radius in centimetres.
+    """
     return ((Mvir * solmass / (200 * rhocrit)) * (3 / (4 * np.pi)))**(1/3)
 
-def Tvir(Mvir):   
+def Tvir(Mvir):
+    """
+    Calculate the virial temperature for a given virial mass.
+
+    Mvir: The virial mass in units of solar masses.
+
+    Returns: The virial temperature in Kelvin.
+    """
     return (mu * mp / (2 * kB)) * (G * Mvir * solmass) / Rvir(Mvir)
 
 ################
@@ -104,6 +138,13 @@ def Tvir(Mvir):
 ################
 
 def Gf(rtw, c, Mvir):
+    """
+    Calculate G(r)=-2*Tvir*a(r), where a(r) is the acceleration profile.
+
+     rtw: The dimensionless radial coordinate r/Rvir.
+       c: The concentration parameter of the halo.
+    Mvir: The virial mass of the halo.
+    """
     x = rtw * c
     return -2 * Tvir(Mvir) * -np.log(1 + x) / (rtw * (np.log(1 + c) - c / (1 + c)))
 
@@ -111,39 +152,80 @@ def Gf(rtw, c, Mvir):
 ## Concentration parameters ##
 ##############################
 
-def make_c_interp(model_str):
-    try:
-        ms, cs = np.loadtxt('data/Mvir_c_{:s}.dat'.format(model_str.title()), unpack=True)
-    except IOError:
-        print('{:s} mass-concentration parameter relation data not found\nAborting...'.format(model_str))
-        exit()
-    else:
-        print('Loading {:s} mass-concentration parameter relation data'.format(model_str.title()))
-    return interpolate.interp1d(ms, cs)
+class MassConcentrationRelation:
+    """
+    Encapsulates a virial mass-concentration parameter relation obtained from a text file containing a list of paired Mvir, c values.
+    Loads the data and constructs an interpolation c(Mvir)
+    """
+
+    def __init__(self, model_str):
+        """
+        Construct a mass-concentration relation.
+        
+        model_str: String containing the name of the data file to load. The path searched is 'data/Mvir_c_' + model_str + '.dat'
+        """
+        try:
+            ms, cs = np.loadtxt('data/Mvir_c_{:s}.dat'.format(model_str.title()), unpack=True)
+        except IOError:
+            print('{:s} mass-concentration parameter relation data not found\nAborting...'.format(model_str))
+            exit(1)
+        else:
+            print('Loading {:s} mass-concentration parameter relation data'.format(model_str.title()))
+            self.__interp = interpolate.interp1d(ms, cs)
+
+    def cparam(self, Mvir):
+        """
+        Return the concentration parameter for a given virial mass.
+        """
+        return self.__interp(Mvir)
 
 #########################
 ## rho_gas calculation ##
 #########################
 
 class Py2StarHelper:
+    """
+    Utility class to unpack a tuple of arguments.
+    Used to emulate the `starmap` function from Python 3 if the code is run with Python 2.
+    """
+    
     def __init__(self, func):
         self.f = func
     def __call__(self, packed_args):
         return self.f(*packed_args)
 
 def kernel(rtw, c, Mvir):
+    """
+    Calculate the density rho which solves to the expression F(rho) - G(r).
+    
+     rtw: The dimensionless radial coordinate r/Rvir.
+       c: The concentration parameter of the halo.
+    Mvir: The virial mass of the halo.
+
+    Returns: The density which solves the expression above for the given parameters
+    """
     rhs = Gf(rtw, c, Mvir)
     cond = lambda lognh: np.abs(Ff(lognh) - rhs)   
     #return 10**optimize.brute(cond, ((log_mean_nh, 0),))[0] * mp
     return 10**optimize.minimize_scalar(cond, bounds=(log_mean_nh, 0), method='bounded').x * mp
 
-py2_kernel = Py2StarHelper(kernel)
+if version_info.major < 3:
+    py2_kernel = Py2StarHelper(kernel)
 
 def rho_gas(rtws, c, Mvir):
+    """
+    Calculate the gas density profile for a particular DM halo.
+
+    rtws: The dimensionless radial coordinates r/Rvir at which to calculate densities. May be a numpy array or a single value.
+       c: The concentration parameter of the halo.
+    Mvir: The virial mass of the halo.
+
+    Returns: A numpy array of densities if an array of radii were provided; a single density otherwise.
+    """
     if isinstance(rtws, np.ndarray):
-        print('Starting numerical run for M={:.3g}, c={:.3g} with {} workers'.format(Mvir, c, N_CPUS))
+        print('Starting calculations for M={:.3g}, c={:.3g} with {} workers'.format(Mvir, c, N_CPUS))
         start_time = time.time()
-        sigint_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
+        sigint_handler = signal.signal(signal.SIGINT, signal.SIG_IGN) # This stops Ctrl-C breaking while multiple processes are running
         worker_pool = multiprocessing.Pool(N_CPUS)
         signal.signal(signal.SIGINT, sigint_handler)
         try:
@@ -155,7 +237,7 @@ def rho_gas(rtws, c, Mvir):
         except KeyboardInterrupt:
             print('Aborting...')
             worker_pool.terminate()
-            exit()
+            exit(0)
         else:
             print('Completed in {:.1f}s'.format(time.time() - start_time))
             worker_pool.close()
@@ -169,6 +251,14 @@ def rho_gas(rtws, c, Mvir):
 ###########################
 
 def M_gas(rads, rhos):
+    """
+    Calculate the total gas mass given a radial density profile.
+
+    rads: Array of radius coordinates in physical units.
+    rhos: Array of density values, at each radius in rads, in physical units.
+
+    Returns: Total gas mass in solar masses.
+    """
     if len(rads):
         return 4 * np.pi * integrate.simps(rads**2 * rhos, rads) / solmass
     else:
@@ -184,6 +274,7 @@ if __name__ == "__main__":
     parser.add_argument('-c' , '--mass-conc-rel', dest='conc', required='true', type=str, help='The mass-concentration relation to use. Either "eagle" or "prada"')
     parser.add_argument('-t' , '--nH-temp-rel', dest='nHT', required='true', type=str, help='The hydrogen density-temperature relation to use. Either "relhic" or "sphcloudy"')
     parser.add_argument('-f', '--force-recalc', dest='force', action='store_true', help='Regenerate density profiles even if data is already found')
+    parser.add_argument('-p', '--plot', dest = 'plot', action='store_true', help='Produce a plot of the gas density profiles when calculations are complete')
     args = parser.parse_args()
 
     fname_ext = '.dat'
@@ -192,14 +283,14 @@ if __name__ == "__main__":
     fname_M = fname_base + '_masses' + fname_ext
     fname_Rv = fname_base + '_Rvirs' + fname_ext
     found_data = os.path.isfile(fname_rho) ^ (args.force)
-    
+
     N_CPUS = multiprocessing.cpu_count()
 
     if not found_data:
         prompt = raw_input('Failed to load data, generate? ')
         if prompt not in set(['yes', 'y', 'Yes']):
             print('Aborting...')
-            exit()
+            exit(0)
         else:
             prompt = raw_input('Number of processes (default = {}) '.format(N_CPUS))
         if prompt is not '':
@@ -207,7 +298,7 @@ if __name__ == "__main__":
                 N_CPUS = int(prompt)
             except:
                 print('Aborting...')
-                exit()
+                exit(1)
     
     if found_data:
         masses = np.loadtxt(fname_M)
@@ -220,16 +311,14 @@ if __name__ == "__main__":
         #############
         ## EDIT ME ##
         #############
-        masses = np.logspace(8, 9.65, 21) # in M_sun   # np.array([10**8])
+        masses = np.logspace(8, 9.65, 21) # in M_sun
         rtws = np.geomspace(1e-3, 30, 1000)
         R200s = Rvir(masses)
 
-    #T200s = Tvir(masses)
-    #np.savetxt('rhos_gas_Rvirs.dat', R200s)
-    c_func = make_c_interp(args.conc)
-    concentrations = c_func(masses)
+    c_rel = MassConcentrationRelation(args.conc)
+    concentrations = c_rel.cparam(masses)
 
-    T_rel = DensityTempRelation(args.nHT)    
+    T_rel = DensityTempRelation(args.nHT)
 
     ##################
     ## Calculations ##
@@ -244,7 +333,9 @@ if __name__ == "__main__":
         np.savetxt(fname_rho, np.column_stack((rtws, rhos_gas.T, T_rel.T(np.log10(rhos_gas / mp)).T)))
         np.savetxt(fname_M, masses)
         np.savetxt(fname_Rv, R200s)
-        
+    elif not args.plot:
+        print('Nothing to do, exiting.')
+        exit(0)
 
     # densities at virial radii
     rhos_R200 = np.array([rho_gas(1, c, Mvir) for c, Mvir in zip(concentrations, masses)])
@@ -265,7 +356,7 @@ if __name__ == "__main__":
     ## Plots ##
     ###########
     
-    if True:
+    if args.plot:
         rtws_within_r200 = rtws[rtws <= 1]
         gas_masses = [M_gas(rtws_within_r200 * rv, rg[rtws < 1]) for rg, rv in zip(rhos_gas, R200s)]
         
